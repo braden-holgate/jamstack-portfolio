e9834ab43f05f6f123a972a34363e002ef3d10a6
braden-holgate <bradenholgate@live.com>
2022-07-26 11:13:32 +1200

    conditional styling nightmare fixed on menu

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -9,14 +9,27 @@ interface Props {
 
 const HamburgerMenu: FC<Props> = ({ isOpen, setIsOpen, home }: Props) => {
   console.log(home)
+  const storecss : string = 'fixed top-5 right-5 xl:hidden '
   return (
     <>
-    <div className={isOpen ? 'flex flex-col w-full min-h-screen pl-5 pr-5 xl:hidden' : ''}>
-      <header className='flex justify-between w-full py-5 border-b border-lines'>
-        <div className={isOpen || !home ? '' : 'hidden'}>braden-holgate</div>
+    {/* menu that displays when not open */}
+      <header className= {
+        !isOpen && !home ? 'flex justify-between pl-5 pr-5 py-5 w-full fixed top-0 border-b border-lines' :
+        !isOpen ? 'flex justify-between pl-5 pr-5 py-5 w-full fixed top-0' : 'hidden'
+        }>
+        <div className={!home ? '' : 'hidden'}>braden-holgate</div>
         <button onClick={() => setIsOpen(!isOpen)} className=''>
           <img className={!isOpen ? 'fixed top-5 right-5 xl:hidden': 'hidden'} src='/menu-line.svg' height={24} width={24} />
-          <img className={isOpen ? '': 'hidden'} src='/close-line.svg' height={24} width={24} />
+        </button>
+      </header>
+
+    {/* menu that displays when open */}
+
+    <div className={isOpen ? 'flex flex-col w-full min-h-screen pl-5 pr-5 xl:hidden' : 'hidden'}>
+      <header className={isOpen ? 'flex justify-between w-full py-5 border-b border-lines' : 'hidden'}>
+        <div className={isOpen ? '' : 'hidden'}>braden-holgate</div>
+        <button onClick={() => setIsOpen(!isOpen)} className=''>
+          <img className={isOpen ? 'fixed top-5 right-5 xl:hidden': 'hidden'} src='/close-line.svg' height={24} width={24} />
         </button>
       </header>
 
